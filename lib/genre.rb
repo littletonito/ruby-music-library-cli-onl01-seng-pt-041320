52f298e28a1b378edc55c190e41417e330c837ad
Antonio Clavijo Cortes <littletonito@gmail.com>
2020-05-26 21:48:32 +0000

    Automatically backed up by Learn

---
+++ lib/genre.rb
@@ -0,0 +1,9 @@
+class Genre 
+  
+  @@all = []
+  
+  def self.all
+    @@all
+  end
+  
+end
---
+++ lib/song.rb
@@ -0,0 +1,34 @@
+class Song 
+  
+  attr_accessor :name
+  attr_reader :artist, :genre
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def artist= (artist)
+    @artist = artist
+  end
+  
+  def genre= (genre)
+    @genre = genre
+  end
+  
+  def self.destroy_all
+    all.clear
+  end
+  
+  def save
+    self.class.all << self
+  end
+  
+  
+  
+end
